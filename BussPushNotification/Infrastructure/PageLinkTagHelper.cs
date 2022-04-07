de943736dc6e97510116a84ac2898780c99eb71b
Dmitriy Listopad <dimalist2005@gmail.com>
2022-04-07 18:00:27 +0300

    Add PageLinkTagHelper for nav element

---
+++ BussPushNotification/Infrastructure/PageLinkTagHelper.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace BussPushNotification.Infrastructure
+{
+    [HtmlTargetElement("nav", Attributes ="page-model")]
+    public class PageLinkTagHelper: TagHelper
+    {
+        private IUrlHelperFactory urlHelperFactory;
+
+        public PageLinkTagHelper(IUrlHelperFactory urlHelperFactory)
+        {
+            this.urlHelperFactory = urlHelperFactory;
+        }
+
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext? ViewContext { get; set; } 
+        public string? PageAction { get; set; }
+        public string? Url { get; set; }
+        public string? TagText { get; set; }
+        public override void Process(TagHelperContext context, 
+            TagHelperOutput output)
+        {
+            if (ViewContext != null)
+            {
+                IUrlHelper urlHelper = urlHelperFactory.GetUrlHelper(ViewContext);
+                TagBuilder result = new TagBuilder("nav");
+                TagBuilder tag = new TagBuilder("a");
+                tag.Attributes["href"] = urlHelper.Action(PageAction, Url);
+                tag.Attributes["text"] = TagText;
+                result.InnerHtml.AppendHtml(tag);
+                output.Content.AppendHtml(result.InnerHtml);
+            }
+           
+        }
+    }
+}
