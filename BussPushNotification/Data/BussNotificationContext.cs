aab083317332ded955ed5eba5beb8e1576382a01
Dmitriy Listopad <dimalist2005@gmail.com>
2022-05-08 22:37:32 +0300

    Create context for connection to data base

---
+++ BussPushNotification/Data/BussNotificationContext.cs
@@ -0,0 +1,14 @@
+﻿using BussPushNotification.Models;
+using Microsoft.EntityFrameworkCore;
+namespace BussPushNotification.Data
+{
+    public class BussNotificationContext : DbContext
+    {
+        public BussNotificationContext(DbContextOptions<BussNotificationContext> options)
+            : base(options)
+        {
+            
+        }
+        public DbSet<User> Users { get; set; } = null!;
+    }
+}
