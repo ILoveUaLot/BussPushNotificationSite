18481c5b2e299bd576e4e05fdb1602701510fb67
Dmitriy Listopad <dimalist2005@gmail.com>
2022-05-13 21:38:57 +0300

    Add model binding and validating model

--- BussPushNotification/Models/User.cs
+++ BussPushNotification/Models/User.cs
@@ -1,10 +1,20 @@
-﻿namespace BussPushNotification.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BussPushNotification.Models
 {
     public class User
     {
         public long? UserID { get; private set; }
+        [Required]
         public string UserName { get; set; } = string.Empty;
+        [Required]
+        [EmailAddress]
         public string UserEmail { get; set; } = string.Empty;
+        [Required]
+        [StringLength(100, MinimumLength = 6)]
         public string UserPassword { get; set; } = string.Empty;
+        [Required]
+        [Compare("UserPassword", ErrorMessage = "Passwords do not match.")]
+        public string RepeatedPassword { get; set; } = string.Empty;
     }
 }
