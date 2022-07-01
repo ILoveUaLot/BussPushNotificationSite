49acc38fb390b72d0867c819005b04a8cb03135b
Dmitriy Listopad <dimalist2005@gmail.com>
2022-07-01 23:51:41 +0300

    remove unused usings

--- BussPushNotification/Models/User.cs
+++ BussPushNotification/Models/User.cs
@@ -1,7 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
-namespace BussPushNotification.Models
+﻿namespace BussPushNotification.Models
 {
     public class User
     {
