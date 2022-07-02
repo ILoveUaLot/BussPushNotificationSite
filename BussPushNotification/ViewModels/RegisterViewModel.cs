53a032852af8760fc0f36b84bdeac57cabcf09f4
Dmitriy Listopad <dimalist2005@gmail.com>
2022-07-03 00:29:47 +0300

    Add regex for password

--- BussPushNotification/ViewModels/RegisterViewModel.cs
+++ BussPushNotification/ViewModels/RegisterViewModel.cs
@@ -1,6 +1,23 @@
-﻿namespace BussPushNotification.ViewModels
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BussPushNotification.ViewModels
 {
     public class RegisterViewModel
     {
+        [Required]
+        public string UserName { get; set; } = string.Empty;
+        [Required]
+        [EmailAddress]
+        public string UserEmail { get; set; }
+        [Required]
+        [StringLength(100, MinimumLength = 6)]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{6,100}$", 
+                            ErrorMessage = "Password must have 1 uppercase letter, 1 lowercase letter, 1 number")]
+        public string UserPassword { get; set; }
+        [Required]
+        [Compare("UserPassword", ErrorMessage = "Passwords do not match")]
+        public string RepeatedPassword { get; set; }
+
     }
 }
