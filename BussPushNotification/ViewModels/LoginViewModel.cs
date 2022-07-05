eccd545b91c80d129ae5d70b3e9f1692be261135
Dmitriy Listopad <dimalist2005@gmail.com>
2022-07-05 15:46:43 +0300

    Add loginVM

---
+++ BussPushNotification/ViewModels/LoginViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BussPushNotification.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string UserName { get; set; } = string.Empty;
+        [Required]
+        public string UserPassword { get; set; } = string.Empty;
+    }
+}
