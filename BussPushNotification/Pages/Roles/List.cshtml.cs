20fea10bdf07868f84b74a437d8e27134602901f
Dmitriy Listopad <dimalist2005@gmail.com>
2022-07-12 01:06:46 +0300

    Add List page of roles

---
+++ BussPushNotification/Pages/Roles/List.cshtml.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BussPushNotification.Pages.Roles
+{
+    public class ListModel : AdminPageModel
+    {
+        public UserManager<IdentityUser> UserManager { get; set; }
+        public RoleManager<IdentityRole> RoleManager { get; set; }
+        public ListModel(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            UserManager = userManager;
+            RoleManager = roleManager;
+        }
+        
+        public IEnumerable<IdentityRole> Roles { get; set; } = Enumerable.Empty<IdentityRole>();
+
+        public void OnGet()
+        {
+            Roles = RoleManager.Roles;
+        }
+        public async Task<string> GetMembersString(string role)
+        {
+            IEnumerable<IdentityUser> users = await UserManager.GetUsersInRoleAsync(role);
+            string result = users.Count() == 0 ? "No members"
+                : string.Join(", ", users.Take(3).Select(u => u.UserName).ToArray());
+            return users.Count() > 3 ? $"{result}, (plus others)" : result;
+        }
+        public async Task<IActionResult> OnPostAsync(string id)
+        {
+            IdentityRole role = await RoleManager.FindByIdAsync(id);
+            await RoleManager.DeleteAsync(role);
+            return RedirectToPage();
+        }
+    }
+}
