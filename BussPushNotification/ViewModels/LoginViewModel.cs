9043c4de17d5f4f8d5504ebc2427362950c104de
Dmitriy Listopad <dimalist2005@gmail.com>
2022-07-14 04:25:16 +0300

    Add returnedUrl

--- BussPushNotification/ViewModels/LoginViewModel.cs
+++ BussPushNotification/ViewModels/LoginViewModel.cs
@@ -1,12 +1,18 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace BussPushNotification.ViewModels
 {
     public class LoginViewModel
     {
+        [BindProperty(SupportsGet = true)]
+        public string? ReturnUrl { get; set; }
         [Required]
+        [BindProperty]
         public string UserName { get; set; } = string.Empty;
         [Required]
+        [BindProperty]
         public string UserPassword { get; set; } = string.Empty;
     }
 }
