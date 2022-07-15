84ebc5938f3d5564072c31e75b687b71cac2ecbb
Dmitriy Listopad <dimalist2005@gmail.com>
2022-07-16 01:18:51 +0300

    add constraints for admin razor pages

--- BussPushNotification/Pages/AdminPageModel.cs
+++ BussPushNotification/Pages/AdminPageModel.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BussPushNotification.Pages
 {
+    [Authorize(Roles = "Admins")]
     public class AdminPageModel : PageModel
     {
     }
