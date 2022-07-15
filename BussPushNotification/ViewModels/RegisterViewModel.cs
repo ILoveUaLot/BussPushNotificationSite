8807d8a90cf186904a8037c1fd3a74861187718d
Dmitriy Listopad <dimalist2005@gmail.com>
2022-07-16 01:19:16 +0300

    Add returnUrl property

--- BussPushNotification/ViewModels/RegisterViewModel.cs
+++ BussPushNotification/ViewModels/RegisterViewModel.cs
@@ -1,19 +1,19 @@
-﻿using System.ComponentModel;
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace BussPushNotification.ViewModels
 {
     public class RegisterViewModel
     {
+        [BindProperty(SupportsGet =true)]
+        public string? ReturnUrl { get; set; }
         [Required]
         public string UserName { get; set; } = string.Empty;
         [Required]
         [EmailAddress]
         public string UserEmail { get; set; }
         [Required]
-        [StringLength(100, MinimumLength = 6)]
-        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{6,100}$", 
-                            ErrorMessage = "Password must have 1 uppercase letter, 1 lowercase letter, 1 number")]
         public string UserPassword { get; set; }
         [Required]
         [Compare("UserPassword", ErrorMessage = "Passwords do not match")]
