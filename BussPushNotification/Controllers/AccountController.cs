6dafd176672dece9c4b8b59d5d07ef94e309afb9
Dmitriy Listopad <dimalist2005@gmail.com>
2022-07-17 03:53:44 +0300

    make profile http get only for authorize

---
+++ BussPushNotification/Controllers/AccountController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BussPushNotification.Controllers
+{
+    public class AccountController : Controller
+    {
+        [Authorize]
+        public IActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
