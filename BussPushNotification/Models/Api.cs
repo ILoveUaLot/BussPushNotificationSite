08412217c8e61691e4f67c72168d04d567aa30b0
Dmitriy Listopad <dimalist2005@gmail.com>
2022-07-26 13:29:16 +0300

    Add api model

---
+++ BussPushNotification/Models/Api.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BussPushNotification.Models;
+
+public partial class Api
+{
+    public string Apiname { get; set; } = null!;
+
+    public string? Apikey { get; set; }
+}
