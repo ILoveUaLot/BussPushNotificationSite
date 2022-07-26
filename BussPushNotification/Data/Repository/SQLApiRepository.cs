10477165352e7603f2bb2f234e9bb57936e88603
Dmitriy Listopad <dimalist2005@gmail.com>
2022-07-26 15:50:35 +0300

    Create class for CRUD api object in db

---
+++ BussPushNotification/Data/Repository/SQLApiRepository.cs
@@ -0,0 +1,51 @@
+﻿using BussPushNotification.Data.Interface;
+using BussPushNotification.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BussPushNotification.Data.Repository
+{
+    public class SQLApiRepository : IApiRepositroy
+    {
+        BussNotificationContext db;
+        public SQLApiRepository(BussNotificationContext db)
+        {
+            this.db = db;
+        }
+
+        public async Task CreateAsync(Api item)
+        {
+            await db.Apis.AddAsync(item);
+        }
+
+        public async Task DeleteAsync(Api item)
+        {
+            db.Remove(item);
+            await Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Api> GetItemAsync(string id)
+        {
+            return await db.Apis.FindAsync(id);
+        }
+
+        public IQueryable<Api> GetList()
+        {
+            return db.Apis.AsQueryable();
+        }
+
+        public async Task SaveAsync()
+        {
+            await db.SaveChangesAsync();
+        }
+
+        public void Update(Api item)
+        {
+            db.Entry(item).State = EntityState.Modified;
+        }
+    }
+}
