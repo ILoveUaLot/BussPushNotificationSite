798d97661b871285da2734bf7782f11fe9951742
Dmitriy Listopad <dimalist2005@gmail.com>
2022-08-04 00:24:00 +0300

    Add route model

---
+++ BussPushNotification/Models/Route.cs
@@ -0,0 +1,17 @@
+﻿namespace BussPushNotification.Models
+{
+    public class Route
+    {
+        public readonly string Title;
+        public readonly string RouteNumber;
+        public readonly string Arrival;
+        public TimeSpan delay { get; set; }
+        public Route(string title, string routeNumber, string arrival, TimeSpan delay)
+        {
+            Title = title;
+            RouteNumber = routeNumber;
+            Arrival = arrival;
+            this.delay = delay;
+        }
+    }
+}
