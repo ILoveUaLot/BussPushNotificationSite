ad11d0786a56d4a507e2b7d36d2289697ccdabc6
Dmitriy Listopad <dimalist2005@gmail.com>
2022-08-06 02:54:03 +0300

    Add default for delay

--- BussPushNotification/Models/Route.cs
+++ BussPushNotification/Models/Route.cs
@@ -5,7 +5,7 @@
         public readonly string Title;
         public readonly string RouteNumber;
         public readonly string Arrival;
-        public TimeSpan delay { get; set; }
+        public TimeSpan delay { get; set; } = TimeSpan.FromMinutes(10);
         public Route(string title, string routeNumber, string arrival, TimeSpan delay)
         {
             Title = title;
