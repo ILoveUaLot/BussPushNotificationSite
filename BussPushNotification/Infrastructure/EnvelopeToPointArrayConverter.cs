b9b18dccf574ae18a7d011cf4af471622229a6b9
Dmitriy Listopad <dimalist2005@gmail.com>
2022-08-09 05:03:10 +0300

    Add converter for deserialization of geodata envelope to Points

---
+++ BussPushNotification/Infrastructure/EnvelopeToPointArrayConverter.cs
@@ -0,0 +1,25 @@
+﻿using BussPushNotification.Data;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace BussPushNotification.Infrastructure
+{
+    public class EnvelopeToPointArrayConverter : JsonConverter<Point[]>
+    {
+        public override Point[]? ReadJson(JsonReader reader, Type objectType, Point[]? existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            JObject jsonObject = JObject.Load(reader);
+            JToken envelope = jsonObject["Envelope"];
+
+            string lowerCorner = envelope["lowerCorner"].ToString();
+            string upperCorner = envelope["upperCorner"].ToString();
+
+            return new[] { new Point(lowerCorner), new Point(upperCorner) };
+        }
+
+        public override void WriteJson(JsonWriter writer, Point[]? value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
