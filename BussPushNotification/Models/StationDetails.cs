733190e16200204cc4e16d57715dc09f0f205aa5
Dmitriy Listopad <dimalist2005@gmail.com>
2022-08-09 03:06:49 +0300

    Add station details

---
+++ BussPushNotification/Models/StationDetails.cs
@@ -0,0 +1,14 @@
+﻿using BussPushNotification.Data;
+using Newtonsoft.Json;
+
+namespace BussPushNotification.Models
+{
+    public class StationDetails
+    {
+        public string Title { get; set; }
+        [JsonProperty("codes")]
+        public Codes Сodes { get; set; }
+
+        public List<string> BussRoutes { get; set; } = new List<string>();
+    }
+}
