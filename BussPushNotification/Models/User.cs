fd30fd501f1454a6dd6ec4613b36d085b88162db
Dmitriy Listopad <dimalist2005@gmail.com>
2023-05-08 22:37:58 +0300

    Connect database

---
+++ BussPushNotification/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace BussPushNotification.Models
+{
+    public class User
+    {
+        public long? UserID { get; private set; }
+        public string UserName { get; set; } = string.Empty;
+        public string UserEmail { get; set; } = string.Empty;
+        public string UserPassword { get; set; } = string.Empty;
+    }
+}
