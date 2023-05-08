96c4217bd3060f22948291763e5062cc3b1de36b
Dmitriy Listopad <dimalist2005@gmail.com>
2023-05-08 23:18:57 +0300

    Add Repository interface

---
+++ BussPushNotification/Data/Interface/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace BussPushNotification.Data.Interface
+{
+    public interface IRepository<T> : IDisposable
+    {
+        IQueryable<T> GetList();
+        T GetItem(int id);
+        void Create(T item);
+        void Update(T item);
+        void Delete(T item);
+        void Save();
+    }
+}
