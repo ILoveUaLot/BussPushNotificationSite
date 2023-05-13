60f2e0771fe8c20b26774bbe8c40e125d817bb62
Dmitriy Listopad <dimalist2005@gmail.com>
2023-05-13 21:38:52 +0300

    Add model binding and validating model

--- BussPushNotification/Controllers/LoginController.cs
+++ BussPushNotification/Controllers/LoginController.cs
@@ -1,16 +1,43 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BussPushNotification.Data;
+using BussPushNotification.Data.Interface;
+using BussPushNotification.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace BussPushNotification.Controllers
 {
     public class LoginController : Controller
     {
-        public IActionResult Index()
+        IRepository<User> db;
+        public LoginController(IRepository<User> repository)
+        {
+            db = repository;
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ViewResult SignUpForm()
         {
             return View();
         }
 
-        public ViewResult SignUpForm()
+        [HttpPost]
+        public ViewResult SignUpForm(User user)
         {
+            if(ModelState.IsValid)
+            {
+                Console.WriteLine("sad");
+                return View("Login");
+            }
+            foreach (var modelState in ModelState.Values)
+            {
+                foreach (var error in modelState.Errors)
+                {
+                    Console.WriteLine(error.ErrorMessage);
+                }
+            }
             return View();
         }
     }
