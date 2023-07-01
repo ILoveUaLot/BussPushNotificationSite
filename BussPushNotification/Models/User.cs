087d6c21fe8b947bcf7f77f81f22bfe871196c20
Dmitriy Listopad <dimalist2005@gmail.com>
2023-07-01 23:31:58 +0300

    Added database option for guid userid

--- BussPushNotification/Models/User.cs
+++ BussPushNotification/Models/User.cs
@@ -1,9 +1,11 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BussPushNotification.Models
 {
     public class User
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public Guid UserID { get; private set; }
         public string UserName { get; set; } = string.Empty;
         public string UserEmail { get; set; } = string.Empty;
