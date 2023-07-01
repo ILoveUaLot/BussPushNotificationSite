c395e3fce7d13d593b4ceb248090e61313c86504
Dmitriy Listopad <dimalist2005@gmail.com>
2023-07-01 23:23:46 +0300

    remove attributes from model and deleate repeated password property

--- BussPushNotification/Models/User.cs
+++ BussPushNotification/Models/User.cs
@@ -4,17 +4,9 @@ namespace BussPushNotification.Models
 {
     public class User
     {
-        public long? UserID { get; private set; }
-        [Required]
+        public Guid UserID { get; private set; }
         public string UserName { get; set; } = string.Empty;
-        [Required]
-        [EmailAddress]
         public string UserEmail { get; set; } = string.Empty;
-        [Required]
-        [StringLength(100, MinimumLength = 6)]
         public string UserPassword { get; set; } = string.Empty;
-        [Required]
-        [Compare("UserPassword", ErrorMessage = "Passwords do not match.")]
-        public string RepeatedPassword { get; set; } = string.Empty;
     }
 }
