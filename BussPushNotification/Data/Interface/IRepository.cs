7d8f80c31f23386c5211308f6d9511b2488ced37
Dmitriy Listopad <dimalist2005@gmail.com>
2023-07-05 16:07:34 +0300

    change parameter for getitem method

--- BussPushNotification/Data/Interface/IRepository.cs
+++ BussPushNotification/Data/Interface/IRepository.cs
@@ -1,9 +1,9 @@
 ﻿namespace BussPushNotification.Data.Interface
 {
-    public interface IRepository<T> : IDisposable
+    public interface IRepository<T, TKey> : IDisposable
     {
         IQueryable<T> GetList();
-        T GetItem(int id);
+        T GetItem(TKey id);
         void Create(T item);
         void Update(T item);
         void Delete(T item);
