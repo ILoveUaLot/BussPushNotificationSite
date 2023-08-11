648070cfff951a352ddf3836abcb8ea02293dd89
Dmitriy Listopad <dimalist2005@gmail.com>
2023-08-11 20:54:12 +0300

    Add dto

---
+++ BussPushNotification/Data/GeodataDTO.cs
@@ -0,0 +1,41 @@
+﻿using BussPushNotification.Infrastructure;
+using Newtonsoft.Json;
+
+namespace BussPushNotification.Data
+{
+    public class GeoMetaData
+    {
+        [JsonProperty("Point")]
+        public Point Center { get; set; }
+
+        [JsonProperty("boundedBy")]
+        public Area Area { get; set; }
+
+        [JsonProperty("text")]
+        public string Address { get; set; }
+    }
+
+    public class Area
+    {
+        [JsonProperty("Envelope")]
+        [JsonConverter(typeof(EnvelopeToPointArrayConverter))]
+        public Point[] Points { get; set; }
+    }
+    public class Point
+    {
+        private string position;
+        public Point([JsonProperty("pos")]string position)
+        {
+            this.position = position;
+        }
+
+        public KeyValuePair<string, string>[] Coordinates
+        {
+            get
+            {
+                var s = position.Split(" ");
+                return new [] { KeyValuePair.Create("latitude", s[0]), KeyValuePair.Create("longitude", s[1]) };
+            }
+        }
+    }
+}
