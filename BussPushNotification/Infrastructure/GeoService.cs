89eb876c853ec74571fad48239d38dd7a17d8f22
Dmitriy Listopad <dimalist2005@gmail.com>
2023-08-18 19:56:37 +0300

    Add service for calculating geo data

---
+++ BussPushNotification/Infrastructure/GeoService.cs
@@ -0,0 +1,23 @@
+﻿using BussPushNotification.Data.Interface;
+
+namespace BussPushNotification.Infrastructure
+{
+    public class GeoService
+    {
+        public double GetRadius(IArea area)
+        {
+            double lowerLatitude = area.LowerCorner.Latitude;
+            double lowerLongitude = area.LowerCorner.Longitude;
+            double upperLatitude = area.UpperCorner.Latitude;
+            double upperLongitude = area.UpperCorner.Longitude;
+            double theta = lowerLatitude - upperLatitude;
+
+            double degree = (Math.PI / 180);
+            double distance = 60 * 1.1515 * (180 / Math.PI) * Math.Acos(
+                Math.Sin(lowerLatitude * degree) * Math.Sin(upperLatitude * degree) +
+                Math.Cos(lowerLatitude * degree) * Math.Cos(upperLatitude * degree) * Math.Cos(theta * degree)
+            );
+            return Math.Round(distance * 1.609344, 2) / 2;
+        }
+    }
+}
