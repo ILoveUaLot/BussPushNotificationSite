be02e862bb6d1d938add417c435cbaaf0b1d6997
Dmitriy Listopad <dimalist2005@gmail.com>
2023-08-18 19:48:11 +0300

    Update geometadata for created abstractions

--- BussPushNotification/Data/GeodataDTO.cs
+++ BussPushNotification/Data/GeodataDTO.cs
@@ -1,4 +1,5 @@
-﻿using BussPushNotification.Infrastructure;
+﻿using BussPushNotification.Data.Interface;
+using BussPushNotification.Infrastructure;
 using Newtonsoft.Json;
 using System.Globalization;
 
@@ -21,58 +22,32 @@ namespace BussPushNotification.Data
             Area = area;
             Address = address;
         }
-
-        public double GetRadius()
-        {
-            double lowerLatitude = Area.GetLowerCorner().Coordinates["latitude"];
-            double lowerLongitude = Area.GetLowerCorner().Coordinates["longitude"];
-            double upperLatitude = Area.GetUpperCorner().Coordinates["latitude"];
-            double upperLongitude = Area.GetUpperCorner().Coordinates["longitude"];
-            double theta = lowerLatitude - upperLatitude;
-
-            double degree = (Math.PI / 180);
-            double distance = 60 * 1.1515 * (180 / Math.PI) * Math.Acos(
-                Math.Sin(lowerLatitude * degree) * Math.Sin(upperLatitude * degree) +
-                Math.Cos(lowerLatitude * degree) * Math.Cos(upperLatitude * degree) * Math.Cos(theta * degree)
-            );
-            return Math.Round(distance * 1.609344, 2) / 2;
-        }
     }
 
-    public class Area
+    public class Area : IArea
     {
-        private readonly Dictionary<string, Point> Corners;
         public Area([JsonProperty("Envelope")][JsonConverter(typeof(EnvelopeToPointArrayConverter))] Point[] points)
         {
-            Corners = new Dictionary<string, Point>
-            {
-                { "lowerCorner", points[0] },
-                { "upperCorner", points[1] }
-            };
-        }
-        public Point GetLowerCorner()
-        {
-            return Corners["lowerCorner"];
+            LowerCorner = points[0];
+            UpperCorner = points[1];
         }
 
-        public Point GetUpperCorner()
-        {
-            return Corners["upperCorner"];
-        }
+        public ICoordinate LowerCorner { get; }
+
+        public ICoordinate UpperCorner { get; }
     }
-    public class Point
+    public class Point : ICoordinate
     {
         public Point([JsonProperty("pos")]string position)
         {
             string[] s = position.Split(" ");
             var s0 = double.Parse(s[0], NumberStyles.Float, CultureInfo.InvariantCulture);
             var s1 = double.Parse(s[1], NumberStyles.Float, CultureInfo.InvariantCulture);
-            Coordinates = new Dictionary<string, double>
-            {
-                {"latitude", s0},
-                {"longitude", s1}
-            };
+            Latitude = s0;
+            Longitude = s1;
         }
-        public readonly Dictionary<string, double> Coordinates;
+
+        public double Latitude { get; }
+        public double Longitude { get; }
     }
 }
