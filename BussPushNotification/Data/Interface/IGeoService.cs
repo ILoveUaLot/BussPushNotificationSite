9579a5fc51854879f59ac125a2cff8608978e860
Dmitriy Listopad <dimalist2005@gmail.com>
2023-08-18 20:08:24 +0300

    Add interface of geoservice

---
+++ BussPushNotification/Data/Interface/IGeoService.cs
@@ -0,0 +1,8 @@
+﻿namespace BussPushNotification.Data.Interface
+{
+    public interface IGeoService
+    {
+        double GetRadius(IArea area);
+        (string country, string settlement, string region) ParsedAddress(string location);
+    }
+}
